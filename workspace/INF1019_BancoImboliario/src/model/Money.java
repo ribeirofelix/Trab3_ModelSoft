80f0da0a2ea8e5c365ac8f3adc77a65945d2c8a2
Felix Ribeiro <ribeiro.felix@live.com>
2012-10-29 20:34:35 -0200

    Money Enum

--- workspace/INF1019_BancoImboliario/src/model/Money.java
+++ workspace/INF1019_BancoImboliario/src/model/Money.java
@@ -2,11 +2,11 @@ package model;
 
 public enum Money {
 	One (1),
-	Five(5),
-	Ten(10),
-	Fifty(50),
-	Hundred(100),
-	FiveHundred(500);
+	Five (5),
+	Ten (10),
+	Fifty (50),
+	Hundred (100),
+	FiveHundred (500);
 	
 	private int value;
 	private Money(int value){
