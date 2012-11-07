b9eaa7d535d7ae10257106a38484062db0407320
Fabio <Mourth>
2012-11-07 00:26:02 -0200

    Correção bug de andar
    Agora ele não se aventura pelo meio do tabuleiro

--- workspace/INF1019_BancoImboliario/src/control/GameController.java
+++ workspace/INF1019_BancoImboliario/src/control/GameController.java
@@ -11,10 +11,14 @@ public class GameController implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Dice dice = new Dice();
+		int sizeOfWalk = 61;
+		
+		/* Roll the dice twice */
 		int numOfHouses = dice.rollTheDice();
 		numOfHouses += dice.rollTheDice();
-		int sizeOfWalk = 67 ;
-		// FIX IT
+		
+		
+		// colocar dado na tela
 		System.out.println(numOfHouses);
 		
 		Player currentPlayer = Main.getCurrentPlayer();		
@@ -27,24 +31,23 @@ public class GameController implements ActionListener {
 			
 			if (currentPlayer.getPosition() >= 0 && currentPlayer.getPosition() < 10){		
 				
-				currentPlayer.setPlayerPoint(new Point(xCoordinate, yCoordinate+sizeOfWalk));				
+				currentPlayer.setPlayerPoint(new Point(xCoordinate, yCoordinate + sizeOfWalk));				
 			}
 			else if (currentPlayer.getPosition() >= 10 && currentPlayer.getPosition() < 20){
 				
-				currentPlayer.setPlayerPoint(new Point(xCoordinate-sizeOfWalk+6, yCoordinate));
+				currentPlayer.setPlayerPoint(new Point(xCoordinate - sizeOfWalk, yCoordinate));
 			}
 			else if (currentPlayer.getPosition() >= 20 && currentPlayer.getPosition() < 30){
 				
-				currentPlayer.setPlayerPoint(new Point(xCoordinate, yCoordinate-sizeOfWalk));
+				currentPlayer.setPlayerPoint(new Point(xCoordinate, yCoordinate - sizeOfWalk));
 			}
-			else if (currentPlayer.getPosition() >= 30 && currentPlayer.getPosition() < 40){
+			else if (currentPlayer.getPosition() >= 30 && currentPlayer.getPosition() <= 39){
 				
-				currentPlayer.setPlayerPoint(new Point(xCoordinate+sizeOfWalk-6, yCoordinate));
+				currentPlayer.setPlayerPoint(new Point(xCoordinate + sizeOfWalk, yCoordinate));
 			}
 			
-			currentPlayer.walkNPositions(1);
+			currentPlayer.walkOnePosition();
 		}
-		
 		
 		Main.nextPlayer();
 		Main.updateFrame();
