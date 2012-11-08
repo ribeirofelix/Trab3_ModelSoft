3c5056a1ade648b640097f5daefe3ac64a0c0b68
Felix Ribeiro <ribeiro.felix@live.com>
2012-11-08 16:07:20 -0200

    imagens pin

--- workspace/INF1019_BancoImboliario/src/model/Pivot.java
+++ workspace/INF1019_BancoImboliario/src/model/Pivot.java
@@ -1,11 +1,19 @@
 package model;
 
 public enum Pivot {
-	White,
-	Orange,
-	Yellow,
-	Green,
-	Red,
-	Blue
+	Black ("images//black_pin.png" ),
+	Orange ("images//orange_pin.png" ),
+	Yellow ( "images//yellow_pin.png"),
+	Purple ( "images//purple_pin.png"),
+	Red ( "images//red_pin.png"),
+	Blue ( "images//blue_pin.png" );
+	
+	private String value;
+	private Pivot(String value){
+		this.value = value;
+	}
+	public String getValue(){
+		return value;
+	}
 
 }
